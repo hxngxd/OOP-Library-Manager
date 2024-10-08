0b5e6c4d56f350eb7868ba6b126de9480fdeb23d
Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>
2024-10-08 15:43:46 +0700

    install jdbc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.hxngxd.library_manager {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires mysql.connector.j;
 
     opens com.hxngxd.library_manager to javafx.fxml;
     exports com.hxngxd.library_manager;
