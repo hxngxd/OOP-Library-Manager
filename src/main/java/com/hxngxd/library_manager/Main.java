01366e635b28e3454a68974e1f2010873c6d7b2c
Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>
2024-10-09 12:50:25 +0700

    sql

--- src/main/java/com/hxngxd/library_manager/Main.java
+++ src/main/java/com/hxngxd/library_manager/Main.java
@@ -1,13 +1,11 @@
 package com.hxngxd.library_manager;
 
 import com.hxngxd.database.DBManager;
-import com.hxngxd.service.UserService;
 
 public class Main {
 
     public static void main(String[] args) {
         boolean connect = DBManager.connect();
-        UserService.loginByEmail("23020078@vnu.edu.vn", "Hung@07112005");
         boolean disconnect = DBManager.disconnect();
     }
 }
