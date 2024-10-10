784e472a429b60ba2de7e803058a487689b9c841
Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>
2024-10-10 23:21:11 +0700

    no more lombok, use log4j for logging

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.hxngxd.libraryManager {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
     requires mysql.connector.j;
+    requires java.sql;
     requires jbcrypt;
-    requires static lombok;
+    requires org.apache.logging.log4j;
 
     opens com.hxngxd.libraryManager to javafx.fxml;
     exports com.hxngxd.libraryManager;
