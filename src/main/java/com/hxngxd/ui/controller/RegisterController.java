2492ca8f8078d577e28035b72022f1e2b6d93af1
hlminh05 <161221667+hlminh05@users.noreply.github.com>
2024-11-05 19:30:26 +0700

    chatvc

--- src/main/java/com/hxngxd/ui/controller/RegisterController.java
+++ src/main/java/com/hxngxd/ui/controller/RegisterController.java
@@ -10,10 +10,13 @@ import com.hxngxd.exceptions.ValidationException;
 import com.hxngxd.service.BookService;
 import com.hxngxd.service.UserService;
 import com.hxngxd.ui.StageManager;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -33,8 +36,60 @@ public class RegisterController extends NavigateController {
     private TextField confirmPasswordField;
     @FXML
     private Label statusLabel;
+    @FXML
+    private TextField passwordVisibleField;
+    @FXML
+    private TextField confirmPasswordVisibleField;
+    @FXML
+    private FontAwesomeIconView eyePassword;
+    @FXML
+    private FontAwesomeIconView eyeConfirmPassword;
 
     private final UserService userService = UserService.getInstance();
+    private boolean isPasswordVisible = false;
+    private boolean isConfirmPasswordVisible = false;
+
+    @FXML
+    private void togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            passwordField.setText(passwordVisibleField.getText());
+            eyePassword.setGlyphName("EYE_SLASH");
+        } else {
+            passwordVisibleField.setText(passwordField.getText());
+            eyePassword.setGlyphName("EYE");
+        }
+        passwordField.setVisible(isPasswordVisible);
+        passwordVisibleField.setVisible(!isPasswordVisible);
+        isPasswordVisible = !isPasswordVisible;
+    }
+
+    @FXML
+    private void toggleConfirmPasswordVisible() {
+        if (isConfirmPasswordVisible) {
+            confirmPasswordField.setText(confirmPasswordVisibleField.getText());
+            eyeConfirmPassword.setGlyphName("EYE_SLASH");
+        } else {
+            confirmPasswordVisibleField.setText(confirmPasswordField.getText());
+            eyeConfirmPassword.setGlyphName("EYE");
+        }
+        confirmPasswordField.setVisible(isConfirmPasswordVisible);
+        confirmPasswordVisibleField.setVisible(!isConfirmPasswordVisible);
+        isConfirmPasswordVisible = !isConfirmPasswordVisible;
+    }
+
+    @FXML
+    private void handleEnterKey(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            register(null);
+            event.consume();
+        }
+    }
+
+    @FXML
+    private void initialize() {
+        confirmPasswordField.setOnKeyPressed(this::handleEnterKey);
+        confirmPasswordVisibleField.setOnKeyPressed(this::handleEnterKey);
+    }
 
     @FXML
     private void register(ActionEvent event) {
