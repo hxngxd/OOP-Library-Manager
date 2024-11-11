4d525c3e4508123addf72530bd1fc5ee145199da
hlminh05 <161221667+hlminh05@users.noreply.github.com>
2024-11-11 21:45:42 +0700

    password toggle for register page (#10)
    * show hide password button onclick
    
    * cleaner password toggle code
    
    * chatvc
    
    * hahaha
    
    * some small mod
    
    ---------
    
    Co-authored-by: Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>

--- src/main/java/com/hxngxd/ui/controller/RegisterController.java
+++ src/main/java/com/hxngxd/ui/controller/RegisterController.java
@@ -10,10 +10,13 @@ import com.hxngxd.exceptions.ValidationException;
 import com.hxngxd.service.BookService;
 import com.hxngxd.service.UserService;
 import com.hxngxd.ui.StageManager;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -33,8 +36,48 @@ public class RegisterController extends NavigateController {
     private TextField confirmPasswordField;
     @FXML
     private Label statusLabel;
+    @FXML
+    private TextField passwordVisibleField;
+    @FXML
+    private TextField confirmPasswordVisibleField;
+    @FXML
+    private FontAwesomeIconView eye;
 
     private final UserService userService = UserService.getInstance();
+    private boolean isPasswordVisible = false;
+
+    @FXML
+    private void togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            passwordField.setText(passwordVisibleField.getText());
+            confirmPasswordField.setText(confirmPasswordVisibleField.getText());
+            eye.setGlyphName("EYE_SLASH");
+        } else {
+            passwordVisibleField.setText(passwordField.getText());
+            confirmPasswordVisibleField.setText(confirmPasswordField.getText());
+            eye.setGlyphName("EYE");
+        }
+        passwordField.setVisible(isPasswordVisible);
+        confirmPasswordVisibleField.setVisible(isPasswordVisible);
+        passwordVisibleField.setVisible(!isPasswordVisible);
+        confirmPasswordVisibleField.setVisible(!isPasswordVisible);
+        isPasswordVisible = !isPasswordVisible;
+    }
+
+
+    @FXML
+    private void handleEnterKey(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            register(null);
+            event.consume();
+        }
+    }
+
+    @FXML
+    private void initialize() {
+        confirmPasswordField.setOnKeyPressed(this::handleEnterKey);
+        confirmPasswordVisibleField.setOnKeyPressed(this::handleEnterKey);
+    }
 
     @FXML
     private void register(ActionEvent event) {
