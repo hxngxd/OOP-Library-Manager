6b44710e886bbcf9fc263e51991932b9fde93959
Nguyen Tuong Hung <141505121+hxngxd@users.noreply.github.com>
2024-11-21 15:44:26 +0700

    remove notification, clean enums

--- src/main/java/com/hxngxd/enums/BorrowStatus.java
+++ src/main/java/com/hxngxd/enums/BorrowStatus.java
@@ -2,12 +2,12 @@ package com.hxngxd.enums;
 
 public enum BorrowStatus {
 
-    PENDING,         // Yêu cầu mượn đang chờ duyệt.
-    APPROVED,        // Yêu cầu đã được phê duyệt.
-    BORROWED,        // Sách đã được mượn.
-    RETURNED_ON_TIME, // Sách trả đúng hạn.
-    OVERDUE,         // Sách đang mượn quá hạn.
-    RETURNED_LATE,   // Sách trả trễ hạn.
-    REJECTED         // Yêu cầu bị từ chối.
-    
+    PENDING,
+    APPROVED,
+    BORROWED,
+    RETURNED_ON_TIME,
+    OVERDUE,
+    RETURNED_LATE,
+    REJECTED
+
 }
